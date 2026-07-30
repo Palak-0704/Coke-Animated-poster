a08c3e1fd11bf27fbb79b72ee2dd855943edd3c5
Palak <palakprasad8283@gmail.com>
2026-07-30 14:09:23 +0530

    Fix 3D model lighting and scale

--- script.js
+++ script.js
@@ -9,11 +9,24 @@ const mindarThree = new MindARThree({
 
 const {renderer, scene, camera}=mindarThree;
 const anchor = mindarThree.addAnchor(0);
+
+//add lighting 
+const ambientLight = new THREE.AmbientLight(0xffffff,2);
+scene.add(ambientLight);
+const directionLight = new THREE.DirectionalLight(0xffffff,3);
+directionLight.position.set(1,2,3);
+scene.add(directionLight);
+
+
 const startBTn = document.querySelector("#startAR");
 const loader= new GLTFLoader();
 
 loader.load("/coca_cola_bottle.glb",(gltf)=>{
     const bottle= gltf.scene;
+    bottle.scale.set(2,2,2);
+    bottle.position.set(0,0,0);
+    
+    bottle.rotation.y=Math.PI;
     anchor.group.add(bottle);
 });
 startBTn.addEventListener("click",async()=>{
